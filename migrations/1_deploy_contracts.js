6fb9a66ced396dc4a8d0096abc39f90bb01a9d72
nsalto <nahuelsalto.71@gmail.com>
2024-01-26 18:37:29 -0300

    add fake usdc for mint

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,11 +1,10 @@
 const CoffeToken = artifacts.require("CoffeToken");
-require("dotenv").config();
-
-const USDC_ADDRESS = process.env.USDC_ADDRESS;
-const MINT_PRICE_IN_WEI = web3.utils.toWei("0.1", "ether");
-console.log("MINT_PRICE_IN_WEI", MINT_PRICE_IN_WEI);
-console.log("USDC_ADDRESS", USDC_ADDRESS) 
+const MockUsdc = artifacts.require("MockUsdc");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(CoffeToken, USDC_ADDRESS, MINT_PRICE_IN_WEI.toString());
+    await deployer.deploy(MockUsdc);
+    const mockUsdcInstance = await MockUsdc.deployed();
+
+    const MINT_PRICE_IN_WEI = "1000000000000000000";
+    await deployer.deploy(CoffeToken, mockUsdcInstance.address, MINT_PRICE_IN_WEI);
 };
