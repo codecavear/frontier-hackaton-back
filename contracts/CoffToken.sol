ef90fdf9b2ded550d6db1178f2d563a22235d00b
nsalto <nahuelsalto.71@gmail.com>
2024-02-05 18:18:55 -0300

    add function to change erc20 address contract

--- contracts/CoffToken.sol
+++ contracts/CoffToken.sol
@@ -6,90 +6,119 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CoffToken is ERC721, Ownable {
-    uint256 private _tokenIds;
+  uint256 private _tokenIds;
 
-    IERC20 public erc20Token;
-    uint256 public mintPrice;
-    mapping(uint256 => uint256) public tokenMintPrices;
-    uint8 public maxOperationLimit = 10;
+  IERC20 public erc20Token;
+  uint256 public mintPrice;
+  mapping(uint256 => uint256) public tokenMintPrices;
+  uint8 public maxOperationLimit = 10;
 
-    constructor(
-        address _erc20Address,
-        uint256 _mintPrice
-    ) ERC721("CoffToken", "COFF") Ownable() {
-        erc20Token = IERC20(_erc20Address);
-        mintPrice = _mintPrice;
-        _tokenIds = 0;
+  constructor(
+    address _erc20Address,
+    uint256 _mintPrice
+  ) ERC721("CoffToken", "COFF") Ownable() {
+    erc20Token = IERC20(_erc20Address);
+    mintPrice = _mintPrice;
+    _tokenIds = 0;
+  }
+
+  function mintToken(
+    uint16 quantity
+  ) public returns (uint256 firstMintedId, uint16 mintedQuantity) {
+    require(
+      quantity > 0 && quantity <= maxOperationLimit,
+      "Invalid quantity or exceeds the maximum limit of quantiy"
+    );
+    require(
+      erc20Token.transferFrom(msg.sender, address(this), mintPrice * quantity),
+      "USD payment failed"
+    );
+
+    firstMintedId = _tokenIds += 1;
+
+    for (uint16 i = 0; i < quantity; i++) {
+      _tokenIds += 1;
+      _mint(msg.sender, _tokenIds);
+      tokenMintPrices[_tokenIds] = mintPrice;
     }
 
-    function mintToken(uint16 quantity) public returns (uint256 firstMintedId, uint16 mintedQuantity) {
-        require(quantity > 0 && quantity <= maxOperationLimit, "Invalid quantity or exceeds the maximum limit of quantiy");        
-        require(
-            erc20Token.transferFrom(msg.sender, address(this), mintPrice * quantity),
-            "USD payment failed"
-        );
+    return (firstMintedId, quantity);
+  }
 
-        firstMintedId = _tokenIds += 1;
-        
-        for(uint16 i = 0; i < quantity; i++) {
-            _tokenIds += 1;
-            _mint(msg.sender, _tokenIds);
-            tokenMintPrices[_tokenIds] = mintPrice;
-        }
+  function redeemToken(uint16[] calldata tokenIds) public {
+    require(
+      tokenIds.length > 0 && tokenIds.length <= maxOperationLimit,
+      "Invalid quantity or exceeds the maximum limit of quantiy"
+    );
 
-        return (firstMintedId, quantity);
+    uint256 totalRedeemAmount = 0;
+
+    for (uint16 i = 0; i < tokenIds.length; i++) {
+      uint16 tokenId = tokenIds[i];
+      require(ownerOf(tokenId) == msg.sender, "You are not the owner");
+
+      uint256 redeemAmount = tokenMintPrices[tokenId];
+      require(redeemAmount > 0, "Error with token mint price");
+
+      totalRedeemAmount += redeemAmount;
+
+      _burn(tokenId);
+      tokenMintPrices[tokenId] = 0;
     }
 
-    function redeemToken(uint16[] calldata tokenIds) public {
-        require(tokenIds.length > 0 && tokenIds.length <= maxOperationLimit, "Invalid quantity or exceeds the maximum limit of quantiy");        
+    require(
+      erc20Token.transfer(msg.sender, totalRedeemAmount),
+      "ERC20 transfer failed"
+    );
+  }
 
-        uint256 totalRedeemAmount = 0;
+  function swapToken(uint16[] calldata tokenIds, address newOwner) public {
+    require(
+      tokenIds.length > 0 && tokenIds.length <= maxOperationLimit,
+      "Invalid quantity or exceeds the maximum limit of quantiy"
+    );
 
-        for(uint16 i = 0; i < tokenIds.length; i++) {
-            uint16 tokenId = tokenIds[i];
-            require(ownerOf(tokenId) == msg.sender, "You are not the owner");
+    for (uint16 i = 0; i < tokenIds.length; i++) {
+      require(
+        ownerOf(tokenIds[i]) == msg.sender,
+        "Only the owner can swap the NFT"
+      );
 
-            uint256 redeemAmount = tokenMintPrices[tokenId];
-            require(redeemAmount > 0, "Error with token mint price");
+      safeTransferFrom(msg.sender, newOwner, tokenIds[i]);
 
-            totalRedeemAmount += redeemAmount;
-
-            _burn(tokenId);
-            tokenMintPrices[tokenId] = 0;
-
-        }
-
-        require(
-            erc20Token.transfer(msg.sender, totalRedeemAmount),
-            "ERC20 transfer failed"
-        );
+      /* group emit */
+      emit NFTsSwapped(msg.sender, newOwner, tokenIds);
     }
+  }
 
-    function swapToken(uint16[] calldata tokenIds, address newOwner) public {
-        require(tokenIds.length > 0 && tokenIds.length <= maxOperationLimit, "Invalid quantity or exceeds the maximum limit of quantiy");        
+  function setMintPrice(uint16 newPrice) public onlyOwner {
+    mintPrice = newPrice;
+  }
 
-        for(uint16 i = 0; i < tokenIds.length; i++) {
-             require(
-                ownerOf(tokenIds[i]) == msg.sender,
-                "Only the owner can swap the NFT"
-            );
+  function setMaxOperationLimit(uint8 newLimit) public onlyOwner {
+    require(newLimit > 0, "The new limit must be greater than 0");
+    maxOperationLimit = newLimit;
+  }
 
-            safeTransferFrom(msg.sender, newOwner, tokenIds[i]);
+  function setERC20Address(address newERC20Address) public onlyOwner {
+    require(
+      newERC20Address != address(0),
+      "ERC20 address cannot be the zero address"
+    );
 
-            /* group emit */
-            emit NFTsSwapped(msg.sender, newOwner, tokenIds);
-        }
+    IERC20 erc20Contract = IERC20(newERC20Address);
+    try erc20Contract.balanceOf(address(this)) returns (uint256) {
+      erc20Token = erc20Contract;
+    } catch {
+      revert(
+        "The provided address does not appear to be a valid ERC20 contract"
+      );
     }
+  }
 
-    function setMintPrice(uint16 newPrice) public onlyOwner {
-        mintPrice = newPrice;
-    }
-
-    function setMaxOperationLimit(uint8 newLimit) public onlyOwner {
-        require(newLimit > 0, "The new limit must be greater than 0");
-        maxOperationLimit = newLimit;
-    }
-
-    event NFTsSwapped(address indexed oldOwner, address indexed newOwner, uint16[] tokenIds);
-
+  event NFTsSwapped(
+    address indexed oldOwner,
+    address indexed newOwner,
+    uint16[] tokenIds
+  );
 }
--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,10 +1,10 @@
 const CoffToken = artifacts.require("CoffToken");
-const MockUsdc = artifacts.require("MockUsdc");
-
+/* const MockUsdc = artifacts.require("MockUsdc");
+ */
 module.exports = async function (deployer) {
-    await deployer.deploy(MockUsdc);
-    const mockUsdcInstance = await MockUsdc.deployed();
+    /* await deployer.deploy(MockUsdc);
+    const mockUsdcInstance = await MockUsdc.deployed(); */
 
     const MINT_PRICE_IN_WEI = "1000000000000000000";
-    await deployer.deploy(CoffToken, mockUsdcInstance.address, MINT_PRICE_IN_WEI);
+    await deployer.deploy(CoffToken, mockUsdcInstance.address || process.env.ERC20_ADDRESS , MINT_PRICE_IN_WEI);
 };
